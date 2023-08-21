4d5a68fc6c189992b343ad01e5d776a5a033bd7b
Dariusz Witkowski <dark9666@gmail.com>
2023-08-21 21:01:09 +0200

    uasfhashjkbhjkmsa

--- src/Company/Enum/ResponseMessageEnum.php
+++ src/Company/Enum/ResponseMessageEnum.php
@@ -7,4 +7,5 @@ class ResponseMessageEnum
     const COMPANY_CREATED_MESSAGE = "Company successfully created";
     const COMPANY_UPDATED_MESSAGE = "Company successfully updated";
     const COMPANY_DELETED_MESSAGE = "Company successfully deleted";
+    const COMPANY_PENIS_MESSAGE = "Company successfully penis";
 }
